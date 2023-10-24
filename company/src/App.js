fd8563d9bafb88b4ebac92fac14e8eac1b411926
Akanksha <akankshaprasad184@gmail.com>
2023-10-24 16:16:25 +0530

    footer

---
+++ company/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Scooter from './components/scooter'
+
+function App() {
+  return (
+    <div> <Scooter/>
+     
+    </div>
+   
+  );
+}
+
+export default App;
